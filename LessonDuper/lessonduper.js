910be9a23c6d36613820749a57ee3c3e13a225d4
Orphanlol <83834491+Orphanlol@users.noreply.github.com>
2023-12-09 14:20:47 -0500

    lol

--- LessonDuper/lessonduper.js
+++ LessonDuper/lessonduper.js
@@ -1,31 +1,6 @@
-var _0x517808 = _0x974e;
-function _0x974e(_0x585d00, _0x51ef6a) {
-  var _0x232f75 = _0x232f();
-  return _0x974e = function (_0x974e13, _0x297dc8) {
-    _0x974e13 = _0x974e13 - 179;
-    var _0x27e033 = _0x232f75[_0x974e13];
-    return _0x27e033;
-  }, _0x974e(_0x585d00, _0x51ef6a);
-}
-(function (_0x3796f0, _0x137fe8) {
-  var _0x3d8260 = _0x974e, _0x6fc5a1 = _0x3796f0();
-  while (true) {
-    try {
-      var _0xef0b6e = parseInt(_0x3d8260(183)) / 1 + -parseInt(_0x3d8260(186)) / 2 * (parseInt(_0x3d8260(181)) / 3) + -parseInt(_0x3d8260(190)) / 4 + -parseInt(_0x3d8260(179)) / 5 + -parseInt(_0x3d8260(184)) / 6 + parseInt(_0x3d8260(187)) / 7 + parseInt(_0x3d8260(182)) / 8;
-      if (_0xef0b6e === _0x137fe8) break; else _0x6fc5a1.push(_0x6fc5a1.shift());
-    } catch (_0x4c9105) {
-      _0x6fc5a1.push(_0x6fc5a1.shift());
-    }
-  }
-}(_0x232f, 376250), console[_0x517808(188)](_0x517808(180)), alert("Lesson duped 3 times."));
-var fg_dfdf_df = document[_0x517808(189)](_0x517808(185)).src;
-open(fg_dfdf_df), open(fg_dfdf_df), open(fg_dfdf_df);
-function _0x232f() {
-  var _0x21fce6 = ["html5Iframe", "104590pDecJB", "2217677NifWWA", "log", "getElementById", "1574164UvhFwa", "2664035JrSNzs", "suck my dick skidders. how about you go skid a bitch & maybe a hoe you fucking moron", "3JDsXSF", "6168160qofRlj", "645361eydXtw", "2269794kZDFFW"];
-  _0x232f = function () {
-    return _0x21fce6;
-  };
-  return _0x232f();
-}
-
-// Shit was ez asf to deobfuscate lmfao. Get a better obfuscator.
+console.log("suck my dick skidders. how about you go skid a bitch & maybe a hoe you fucking moron");
+alert("Lesson duped 3 times.");
+var fg_dfdf_df = document.getElementById("html5Iframe").src;
+open(fg_dfdf_df);
+open(fg_dfdf_df);
+open(fg_dfdf_df); // Shit was ez asf to deobfuscate lmfao. Get a better obfuscator.
